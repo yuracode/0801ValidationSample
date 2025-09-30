a1ce313330f1b833e48ce508aee12d2c90f600bb
Hiroyuki Sasaki <hry-sasaki@yoshida-g.ac.jp>
2025-09-30 17:04:55 +0900

    first commit

---
+++ src/main/java/com/example/demo/controller/ValidationController.java
@@ -0,0 +1,63 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.form.CalcForm;
+import com.example.demo.validator.CalcValidator;
+
+import lombok.RequiredArgsConstructor;
+
+//▽▽▽▽▽ リスト8.15 ▽▽▽▽▽
+@Controller
+@RequiredArgsConstructor
+public class ValidationController {
+	/** インジェクション */
+	private final CalcValidator calcValidator;
+
+	/** 相関チェック登録 */
+	@InitBinder("calcForm")
+	public void initBinder(WebDataBinder webDataBinder) {
+		webDataBinder.addValidators(calcValidator);
+	}
+// △△△△△ リスト8.15 △△△△△
+	
+	/** 「form-backing bean」の初期化 */
+	@ModelAttribute
+	public CalcForm setUpForm() {
+		return new CalcForm();
+	}
+
+	/** 入力画面を表示する */
+	@GetMapping("show")
+	public String showView() {
+		// 戻り値は「ビュー名」を返す
+		return "entry";
+	}
+	
+	/** 確認画面を表示する：Formクラス使用 */
+	@PostMapping("calc")
+	public String confirmView(@Validated CalcForm form,
+	        BindingResult bindingResult, Model model) {
+	    // 入力チェック
+	    if (bindingResult.hasErrors()) {
+	        // 入力チェックNG
+	        // 入力画面へ
+	        return "entry";
+	    }
+	    // 入力チェックOK
+	    // 加算実行
+	    Integer calcResult = form.getLeftNum() + form.getRightNum();
+	    // Modelに格納する
+	    model.addAttribute("result", calcResult);
+	    // 確認画面へ
+	    return "confirm";
+	}
+}
---
+++ src/main/java/com/example/demo/form/CalcForm.java
@@ -0,0 +1,16 @@
+package com.example.demo.form;
+
+import org.hibernate.validator.constraints.Range;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class CalcForm {
+	@NotNull(message = "左：数値が未入力です。")
+	@Range(min = 1, max = 10, message = "左：{min}～{max}の数値を入力して下さい。")
+	private Integer leftNum;
+	@NotNull(message = "右：数値が未入力です")
+	@Range(min = 1, max = 10, message = "右：{min}～{max}の数値を入力して下さい。")
+	private Integer rightNum;
+}
