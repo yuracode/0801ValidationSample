18c1cc9d102d43ba447de02fffab5a97f53b4b01
Hiroyuki Sasaki <hry-sasaki@yoshida-g.ac.jp>
2025-10-16 15:18:15 +0900

    実習用データへ加工

--- src/main/java/com/example/demo/controller/ValidationController.java
+++ src/main/java/com/example/demo/controller/ValidationController.java
@@ -4,14 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.form.CalcForm;
-import com.example.demo.validator.CalcValidator;
 
 import lombok.RequiredArgsConstructor;
 
@@ -20,26 +16,26 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ValidationController {
 	/** インジェクション */
-	private final CalcValidator calcValidator;
+//	private final CalcValidator calcValidator;
 
 	/** 相関チェック登録 */
-	@InitBinder("calcForm")
-	public void initBinder(WebDataBinder webDataBinder) {
-		webDataBinder.addValidators(calcValidator);
-	}
+//	@InitBinder("calcForm")
+//	public void initBinder(WebDataBinder webDataBinder) {
+//		webDataBinder.addValidators(calcValidator);
+//	}
 // △△△△△ リスト8.15 △△△△△
 	
 	/** 「form-backing bean」の初期化 */
-	@ModelAttribute
-	public CalcForm setUpForm() {
-		return new CalcForm();
-	}
+//	@ModelAttribute
+//	public CalcForm setUpForm() {
+//		return new CalcForm();
+//	}
 
 	/** 入力画面を表示する */
 	@GetMapping("show")
 	public String showView() {
 		// 戻り値は「ビュー名」を返す
-		return "entry";
+		return "entry1";
 	}
 	
 	/** 確認画面を表示する：Formクラス使用 */
@@ -47,11 +43,11 @@ public class ValidationController {
 	public String confirmView(@Validated CalcForm form,
 	        BindingResult bindingResult, Model model) {
 	    // 入力チェック
-	    if (bindingResult.hasErrors()) {
-	        // 入力チェックNG
-	        // 入力画面へ
-	        return "entry";
-	    }
+//	    if (bindingResult.hasErrors()) {
+//	        // 入力チェックNG
+//	        // 入力画面へ
+//	        return "entry";
+//	    }
 	    // 入力チェックOK
 	    // 加算実行
 	    Integer calcResult = form.getLeftNum() + form.getRightNum();
--- src/main/java/com/example/demo/validator/CalcValidator.java
+++ src/main/java/com/example/demo/validator/CalcValidator.java
@@ -1,31 +1,25 @@
 package com.example.demo.validator;
 
-import org.springframework.stereotype.Component;
-import org.springframework.validation.Errors;
-import org.springframework.validation.Validator;
+//@Component
+//public class CalcValidator implements Validator {
+//	@Override
+//	public boolean supports(Class<?> clazz) {
+//		// 引数で渡されたFormが入力チェックの対象かを論理値で返します
+//		return CalcForm.class.isAssignableFrom(clazz);
+//	}
 
-import com.example.demo.form.CalcForm;
-
-@Component
-public class CalcValidator implements Validator {
-	@Override
-	public boolean supports(Class<?> clazz) {
-		// 引数で渡されたFormが入力チェックの対象かを論理値で返します
-		return CalcForm.class.isAssignableFrom(clazz);
-	}
-
-	@Override
-	public void validate(Object target, Errors errors) {
-		// 対象のFormを取得する
-		CalcForm form = (CalcForm) target;
-		// 値が入っているかの判定
-		if (form.getLeftNum() != null && form.getRightNum() != null) {
-			// (左側の入力項目が「奇数」かつ右側の入力項目が「偶数」)でない
-			if (!((form.getLeftNum() % 2 == 1) && (form.getRightNum() % 2 == 0))) {
-				// エラーの場合は、直接エラーメッセージを指定する
-				errors.rejectValue("leftNum", null,
-						"左側の数値は奇数、右側の数値は偶数でなければなりません。");
-			}
-		}
-	}
-}
+//	@Override
+//	public void validate(Object target, Errors errors) {
+//		// 対象のFormを取得する
+//		CalcForm form = (CalcForm) target;
+//		// 値が入っているかの判定
+//		if (form.getLeftNum() != null && form.getRightNum() != null) {
+//			// (左側の入力項目が「奇数」かつ右側の入力項目が「偶数」)でない
+//			if (!((form.getLeftNum() % 2 == 1) && (form.getRightNum() % 2 == 0))) {
+//				// エラーの場合は、直接エラーメッセージを指定する
+//				errors.rejectValue("leftNum", null,
+//						"左側の数値は奇数、右側の数値は偶数でなければなりません。");
+//			}
+//		}
+//	}
+//}
